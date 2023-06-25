14b7400d8eccd8c80afa1787405547fe9910445c
Məhəmməd Cəfərli <mehemmed.cf@gmail.com>
2023-06-25 22:07:27 +0400

    Task B added

--- Program - Köçür.cs
+++ Program - Köçür.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.ComponentModel.Design;
 using System.Net.WebSockets;
+using System.Numerics;
 
 namespace tasks
 {
@@ -8,10 +9,11 @@ namespace tasks
     {
         static void Main(string[] args)
         {
-            //A
-            bool IsContinue = true;
+            // Task-A
 
-            while (IsContinue)
+           /* bool IsContinue = true;
+
+            while (IsContinue) // n
             {
                 Console.WriteLine("  birinci ededinizi daxil edin  ");
                 int num1 = Convert.ToInt32(Console.ReadLine());
@@ -19,7 +21,7 @@ namespace tasks
                 Console.WriteLine("  ikinci ededinizi daxil edin  ");
                 int num2 = Convert.ToInt32(Console.ReadLine());
 
-                if (num1 > num2)
+                if (num1 > num2) // n
                 {
                     Console.WriteLine("  ededleriniz yanlisdir , xais olunur yeniden cehd edin  ");
                     continue;
@@ -27,15 +29,37 @@ namespace tasks
 
                 int sum = 0;
 
-                for (int i = num1; i <= num2; i++)
+                for (int i = num1; i <= num2; i++) // n
                 {
                     sum += i;
                 }
 
                 Console.WriteLine(sum);
-            }
-            //B
 
-        }
+                // big-O = 0(3n)
+
+            } */
+
+
+            // Task-B
+            Console.WriteLine("neçe edede ehtiyacin var kele?");
+            int num = Convert.ToInt32(Console.ReadLine());
+
+            int[] ededler = new int[num];
+
+            for (int i = 0; i < num; i++) //n
+            {
+                Console.Write($"{i + 1}.eded:");
+                ededler[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            double ortalama = ededler.Sum()/(double)num;
+            Console.WriteLine("ortalama:" + ortalama);
+
+        } // big-O = o(n)
+
+
+          // Task-C
+
     }
 }
